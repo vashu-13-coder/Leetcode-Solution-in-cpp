dbf47a4e529f2ed8361e8607b865fc9200179d6d
vashu-13-coder <vashugupta142536@gmail.com>
2026-08-24 23:12:56 +0530

    Time: 2 ms (77.37%), Space: 7.8 MB (38.83%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int fib(int n) {
-        //Base Case
-        if(n==0){
-            return 0;
+        if (n <= 1) {
+            return n;
         }
-        else if (n == 1){
-            return 1;
+        int a1 = 0, a2 = 1, a3 = 0;
+        for (int i = 2; i <= n; i++) {
+            a3 = a1 + a2;
+            a1 = a2;
+            a2 = a3;
         }
-
-        //Recursive Relation
-        return fib(n-1)+fib(n-2);
+        return a3;
     }
 };
